2ac0969627c24a5860033c53758749adec125a0f
LMFront-end <perfilgitlinamaria@gmail.com>
2022-02-20 20:48:30 -0500

    se termina el controller project y role

--- src/main/java/com/exampleJpaHibernate/demoEmployee/controllers/ProjectController.java
+++ src/main/java/com/exampleJpaHibernate/demoEmployee/controllers/ProjectController.java
@@ -1,14 +1,12 @@
 package com.exampleJpaHibernate.demoEmployee.controllers;
 
 import com.exampleJpaHibernate.demoEmployee.models.Project;
-import com.exampleJpaHibernate.demoEmployee.models.Role;
 import com.exampleJpaHibernate.demoEmployee.services.ProjectService;
-import com.exampleJpaHibernate.demoEmployee.services.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/project")
@@ -17,10 +15,36 @@ public class ProjectController {
     @Autowired
     ProjectService projectService;
 
+    // obtener todos los projects
+    @GetMapping
+    public List<Project> findAll() {
+        return projectService.findAll();
+    }
+
+    // obtener datos por id
+    //http://localhost:8080/api/project/id={id}
+    public Optional<Project> findById(@PathVariable Long id){
+        return projectService.findById(id);
+    }
+
+    // actualizar project
+    //http://localhost:8080/api/project/update
+    @PutMapping("/update")
+    public Project updateProject(@RequestBody Project project){
+        return projectService.updateProject(project);
+    }
+
     @PostMapping("/create")
     //http://localhost:8080/api/project/create
     //@RequestBody -> convierte JSON a una instacia de clase
     public Project createProject(@RequestBody Project project){
         return projectService.saveProject(project);
+    }
+
+    // eliminar project
+    //http://localhost:8080/api/project/delete/{id}
+    @DeleteMapping("/delete/{id}")
+    public void deleteById(@PathVariable Long id){
+        projectService.deleteById(id);
     }
 }
--- src/main/java/com/exampleJpaHibernate/demoEmployee/controllers/RoleController.java
+++ src/main/java/com/exampleJpaHibernate/demoEmployee/controllers/RoleController.java
@@ -1,12 +1,14 @@
 package com.exampleJpaHibernate.demoEmployee.controllers;
 
 
+import com.exampleJpaHibernate.demoEmployee.models.Employee;
 import com.exampleJpaHibernate.demoEmployee.models.Role;
 import com.exampleJpaHibernate.demoEmployee.services.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/role")
@@ -15,6 +17,26 @@ public class RoleController {
     @Autowired
     RoleService roleService;
 
+    // obtener todos los roles
+    @GetMapping
+    public List<Role> findAll() {
+        return roleService.findAll();
+    }
+
+    // obtener datos por id
+    //http://localhost:8080/api/role/id={id}
+    public Optional<Role> findById(@PathVariable Long id){
+        return roleService.findById(id);
+    }
+
+    // actualizar role
+    //http://localhost:8080/api/role/update
+    @PutMapping("/update")
+    public Role updateRole(@RequestBody Role role){
+        return roleService.updateRole(role);
+    }
+
+    // crear role
     @PostMapping("/create")
     //http://localhost:8080/api/role/create
     //@RequestBody -> convierte JSON a una instacia de clase
@@ -22,6 +44,12 @@ public class RoleController {
         return roleService.saveRole(role);
     }
 
+    // eliminar role
+    //http://localhost:8080/api/role/delete/{id}
+    @DeleteMapping("/delete/{id}")
+    public void deleteById(@PathVariable Long id){
+        roleService.deleteById(id);
+    }
 
 
 }
